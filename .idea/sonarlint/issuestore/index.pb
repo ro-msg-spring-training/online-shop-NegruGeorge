292dc30632efe19e0b059747e0a3eb101fa023eb
Negru George <negru.george8@yahoo.com>
2021-04-08 10:14:15 +0300

    cp 2 H2 config

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+?
+shop/.gitignore,7\8\7880a59ab4701c84d1ed8fdb464c591a98bcec6a
+9
+	shop/mvnw,b\3\b3ef7eca62221c0e86d56eb8208bc67b9ab8567f
+h
+8shop/src/main/java/com/example/shop/ShopApplication.java,a\3\a378266f6134131ce5220f69dee66c624663f24b
+<
+shop/pom.xml,1\2\127ce03ec0f5429f885be0e31b01e203e95c1a03
---
+++ shop/src/main/java/com/example/shop/ShopApplication.java
@@ -0,0 +1,18 @@
+package com.example.shop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		System.out.println("sssss");
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+
+
+}
